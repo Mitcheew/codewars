9cb13fe61155cb2d655f57b1700c34b6fe55838a
Mitcheew <wowmormon@gmail.com>
2020-02-08 11:24:57 -0700

    count monkeys

---
+++ Count the Monkeys!.js
@@ -0,0 +1,9 @@
+const monkeyCount = n => 
+{
+  let answer = []
+  for( let i = 0; i < n; i++){
+    answer[i] = i + 1;
+  }
+  return answer;
+}
+//https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
