a5d5e342a4ca86797ca640c36f16b41955461b9c
Mitcheew <wowmormon@gmail.com>
2020-03-12 15:15:52 -0600

    sum of numbers

---
+++ summation.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+const summation = (n) => {
+    let total = 0;
+    for(let i = 1; i <= n; i++){
+      total +=i;
+    }
+    return total
+  };
